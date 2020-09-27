0e15a7073526294a481c7f9d165a6b57dde2687f
AnastaciaVolkova <polarbolide@gmail.com>
2020-09-27 20:15:11 +0200

    chore: Remove nutrition from common folders

--- src/nutrition.cpp
+++
@@ -1,9 +0,0 @@
-#include "nutrition.hpp"
-
-float Nutrition::GetKCProtein(float grams){return grams * kc4g_protein;};
-float Nutrition::GetKCFat(float grams){return grams * kc4g_fat;};
-float Nutrition::GetKCCarb(float grams){return grams * kc4g_carbohydrate;};
-
-float Nutrition::GetGrProtein(float kcal){return kcal / kc4g_protein;};
-float Nutrition::GetGrFat(float kcal){return kcal / kc4g_fat;};
-float Nutrition::GetGrCarb(float kcal){return kcal / kc4g_carbohydrate;};
