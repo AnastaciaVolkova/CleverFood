495b5a26c249f2e5f19f6f109adc44c18d718c4a
AnastaciaVolkova <polarbolide@gmail.com>
2020-09-27 22:53:32 +0200

    feat: Use Initialize method to read from database in product controller constructor

--- src/product/product_ctrl.cpp
+++ src/product/product_ctrl.cpp
@@ -14,13 +14,14 @@ ProductCtrl::ProductCtrl(string file) {
     model_ = std::make_unique<ProductMdl>(this);
     ifstream ifs(file);
     string line;
+    vector<vector<string>> records;
     while (getline(ifs, line)) {
         istringstream iss(line);
         vector<string> tokens(std::istream_iterator<string>{iss}, std::istream_iterator<string>());
-        if (tokens.size() == 4)
-            model_->AddProduct(tokens[0], stof(tokens[1]), stof(tokens[2]), stof(tokens[3]));
+        records.push_back(tokens);
     }
     ifs.close();
+    model_->Inititalize(records);
 };
 
 void ProductCtrl::SetView(ProductView* view) { view_ = view; };
