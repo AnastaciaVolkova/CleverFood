0e1223912838c83073d37cf8d4de344facbfe667
AnastaciaVolkova <polarbolide@gmail.com>
2020-09-27 21:28:00 +0200

    style: Use linux style endings

--- include/product/product_ctrl_iv.hpp
+++ include/product/product_ctrl_iv.hpp
@@ -1,5 +1,12 @@
 #ifndef PRODUCT_CTRL_IV_HPP
 #define PRODUCT_CTRL_IV_HPP
+
+#include <memory>
+
+class ProductView;
+
 class IVProductCtrl {
+public:
+    virtual void SetView(ProductView* view) = 0;
 };
 #endif
