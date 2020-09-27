3b2f0d48c47ace90c169f9675954af53dcae0fe7
AnastaciaVolkova <polarbolide@gmail.com>
2020-09-27 21:53:56 +0200

    style: Improve indentation

--- include/product/product.hpp
+++ include/product/product.hpp
@@ -11,43 +11,43 @@
 /*!
 \class Class stores information on product
 */
-class Product{
+class Product {
 private:
-  float fat_gr_, protein_gr_, carbohydrate_gr_;
-  
-  std::string name_;  // Name of the product.
+    float fat_gr_, protein_gr_, carbohydrate_gr_;
+
+    std::string name_;  // Name of the product.
 public:
-  //! Default constructor
-  Product() = default;
+    //! Default constructor
+    Product() = default;
 
-  //! Constructor
-  /*
-  \param [in] name name of the product
-  \param [in] p protein in grammms
-  \param [in] f fat in gramms
-  \param [in] c carbohydrate in gramms
-  */
-  Product(std::string name, float p, float f, float c);  
+    //! Constructor
+    /*
+    \param [in] name name of the product
+    \param [in] p protein in grammms
+    \param [in] f fat in gramms
+    \param [in] c carbohydrate in gramms
+    */
+    Product(std::string name, float p, float f, float c);
 
-  //! Less operator
-  bool operator< (const Product& other) const;
-  
-  //! Getter of product name
-  std::string GetName();
+    //! Less operator
+    bool operator< (const Product& other) const;
 
-  //! Getter of product calories 
-  /*
-  \return returns kilocallories
-  */
-  float GetKCalories();
+    //! Getter of product name
+    std::string GetName();
 
-  //! Getter for protein in grams
-  float GetProteinGr();
+    //! Getter of product calories 
+    /*
+    \return returns kilocallories
+    */
+    float GetKCalories();
 
-  //! Getter for fat in grams
-  float GetFatGr();
+    //! Getter for protein in grams
+    float GetProteinGr();
 
-  //! Getter for carbohydrates in grams
-  float GetCarbGr();
+    //! Getter for fat in grams
+    float GetFatGr();
+
+    //! Getter for carbohydrates in grams
+    float GetCarbGr();
 };
 #endif PRODUCT_HPP_
--- src/product/product.cpp
+++ src/product/product.cpp
@@ -13,20 +13,20 @@
 
 using std::string;
 
-Product::Product(string name, float p, float f, float c): name_(name), fat_gr_(f), protein_gr_(p), carbohydrate_gr_(c){};
+Product::Product(string name, float p, float f, float c) : name_(name), fat_gr_(f), protein_gr_(p), carbohydrate_gr_(c) {};
 
 bool Product::operator< (const Product& other) const { return name_ < other.name_; };
 
-float Product::GetKCalories(){
-    return (Nutrition::GetKCProtein(protein_gr_)+
-            Nutrition::GetKCFat(fat_gr_) +
-            Nutrition::GetKCCarb(carbohydrate_gr_));
+float Product::GetKCalories() {
+    return (Nutrition::GetKCProtein(protein_gr_) +
+        Nutrition::GetKCFat(fat_gr_) +
+        Nutrition::GetKCCarb(carbohydrate_gr_));
 };
 
-float Product::GetProteinGr(){return protein_gr_;};
+float Product::GetProteinGr() { return protein_gr_; };
 
-float Product::GetFatGr(){return fat_gr_;};
+float Product::GetFatGr() { return fat_gr_; };
 
-float Product::GetCarbGr(){return carbohydrate_gr_;};
+float Product::GetCarbGr() { return carbohydrate_gr_; };
 
 std::string Product::GetName() { return name_; };
