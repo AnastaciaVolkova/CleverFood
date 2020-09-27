9631c450da1a694e17ee14a582e9dd8ce651c6e7
AnastaciaVolkova <polarbolide@gmail.com>
2020-09-27 22:04:28 +0200

    feat: Add GetProducts method

--- include/product/product_mdl.hpp
+++ include/product/product_mdl.hpp
@@ -4,6 +4,7 @@
 #include "product/product.hpp"
 #include <set>
 #include <string>
+#include <vector>
 
 class ProductMdl {
 private:
@@ -21,5 +22,11 @@ public:
     * \return Return true if insertion of record was successful.
     */
     bool AddProduct(std::string name, float p, float f, float c);
+
+    /*!
+    * \brief Write to method parameters all records.
+    * \params products [out] records with all products {name, protein, fet, carbohydrate}
+    */
+    void GetProducts(std::vector<std::vector<std::string>>& products);
 };
 #endif
--- src/product/product_mdl.cpp
+++ src/product/product_mdl.cpp
@@ -1,6 +1,18 @@
 #include "product/product_mdl.hpp"
 
+using std::to_string;
+
 bool ProductMdl::AddProduct(std::string name, float p, float f, float c) {
     auto pr = products_.emplace(name, p, f, c);
     return pr.second;
+};
+
+void ProductMdl::GetProducts(std::vector<std::vector<std::string>>& products) {
+    products.clear();
+    for (auto record : products_) {
+        products.push_back({ record.GetName(),
+            to_string(record.GetProteinGr()),
+            to_string(record.GetFatGr()),
+            to_string(record.GetCarbGr()) });
+    }
 };
