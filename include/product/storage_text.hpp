c9fbebbed9baef53d598e95543274650119ffae5
AnastaciaVolkova <polarbolide@gmail.com>
2020-10-03 13:14:50 +0200

    feat: Realize Open and Close function

--- include/product/storage_text.hpp
+++ include/product/storage_text.hpp
@@ -1,21 +1,28 @@
 #ifndef STORAGE_TEST_HPP_
 #define STORAGE_TEST_HPP_
 #include "storage_i.hpp"
+#include <fstream>
+
 class StorageText : public StorageI {
 private:
     std::string file_name_;
+    std::fstream fs_;
 public:
+
     /*!
     * \brief Constructor for storage.
     * \param file Full storage file name.
     */
     StorageText(std::string file);
+
     /*!
     * \brief Function opens storage.
     * \return returns true if storage was opened successfully.
     */
     bool Open() override;
-    bool Close() override;
+
+    //! Close connection.
+    void Close() override;
     bool Save() override;
     bool Update() override;
     bool Add() override;
--- src/product/storage_text.cpp
+++ src/product/storage_text.cpp
@@ -1,19 +1,26 @@
 #include "product/storage_text.hpp"
 
 using std::string;
+using std::fstream;
 
-StorageText::StorageText(string file):file_name_(file) {};
+StorageText::StorageText(string file) :file_name_(file) {};
 
-bool StorageText::Open(){ return true; };
+bool StorageText::Open() {
+    if (!fs_.is_open())
+        fs_.open(file_name_, std::ifstream::in);
+    return fs_.is_open();
+};
 
-bool StorageText::Close(){ return true; };
+void StorageText::Close() {
+    fs_.close();
+};
 
-bool StorageText::Save(){ return true; };
+bool StorageText::Save() { return true; };
 
-bool StorageText::Update(){ return true; };
+bool StorageText::Update() { return true; };
 
 bool StorageText::Add() { return true; };
 
 bool StorageText::Delete() { return true; };
 
-StorageText::~StorageText(){};
+StorageText::~StorageText() {};
