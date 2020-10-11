802832e51917bafe1ab731e413bece1d721c75be
AnastaciaVolkova <polarbolide@gmail.com>
2020-10-11 12:02:56 +0200

    test: Add unit test

--- product/test/clever_food_ut.cpp
+++ product/test/clever_food_ut.cpp
@@ -2,43 +2,155 @@
 #include "product_ctrl_iv.hpp"
 #include "product_ctrl.hpp"
 #include "storage_text.hpp"
+#include "product_view_i.hpp"
+#include <vector>
+#include <map>
+#include <sstream>
+#include <algorithm>
+#include <initializer_list>
 
-//enum class Parameter { protein, fet, carbohydrate };
 //virtual void SetView(ProductView* view) = 0;
 //virtual void Show() = 0;
 //virtual void Save() = 0;
-//virtual bool AddProduct(std::string name, std::string p, std::string f, std::string c) = 0;
-//virtual bool UpdateProduct(std::string name, Parameter parameter, std::string meaning) = 0;
 //virtual bool DeleteProduct(std::string name) = 0;
 //virtual bool CheckName(std::string name) = 0;
-//virtual bool CheckFat(std::string meaning) = 0;
-//virtual bool CheckCarbo(std::string meaning) = 0;
 //virtual ~IVProductCtrl() {};
 
-class IVProductTest : public ::testing::Test {
-protected:
-    void SetUp() override {
-        product_ctrl = std::make_unique<ProductCtrl>(std::move(std::make_unique<StorageText>("./test_data.txt")));
-    };
-    std::unique_ptr<IVProductCtrl> product_ctrl;
+struct MyMap {
+    using ViewOut = std::map<std::string, std::vector<float>>;
+    ViewOut data;
+    MyMap() = default;
+    MyMap(std::initializer_list<std::pair<const std::string, std::vector<float>>> x) :data{ x } {};
+    bool operator==(const MyMap& ot) const {
+        if (data.size() != ot.data.size()) return false;
+        for (auto k : data) {
+            if (ot.data.find(k.first) == ot.data.end()) return false;
+            auto it = k.second.begin();
+            auto it_ot = ot.data.at(k.first).begin();
+            float diff = 0;
+            while (it != k.second.end()) {
+                diff += fabsf(*it - *it_ot);
+                it++;
+                it_ot++;
+            }
+            if (diff > 1e-3) return false;
+        }
+        return true;
+    }
 };
 
-TEST_F(IVProductTest, CheckProtein) {
-    EXPECT_TRUE(product_ctrl->CheckProtein("3.4"));
-    EXPECT_TRUE(product_ctrl->CheckProtein("3.48"));
-    EXPECT_TRUE(product_ctrl->CheckProtein("39.57"));
-    EXPECT_TRUE(product_ctrl->CheckProtein("12.7"));
-    EXPECT_TRUE(product_ctrl->CheckProtein("4"));
-    EXPECT_TRUE(product_ctrl->CheckProtein(".5"));
-    EXPECT_TRUE(product_ctrl->CheckProtein("6."));
+class UnitTestView :public IProductView {
+    std::string output_str;
+public:
+    void Run() override {};
 
-    EXPECT_TRUE(product_ctrl->CheckProtein("3,4"));
-    EXPECT_TRUE(product_ctrl->CheckProtein("3,48"));
-    EXPECT_TRUE(product_ctrl->CheckProtein("39,57"));
-    EXPECT_TRUE(product_ctrl->CheckProtein("12,7"));
-    EXPECT_TRUE(product_ctrl->CheckProtein("4"));
-    EXPECT_TRUE(product_ctrl->CheckProtein(",5"));
-    EXPECT_TRUE(product_ctrl->CheckProtein("6,"));
+    void Show(std::vector<std::vector<std::string>> records) override {
+        output_str = "";
+        for (auto line : records) {
+            for (auto cell : line)
+                output_str += (cell + " ");
+            output_str[output_str.size() - 1] = '\n';
+        }
+    };
+
+    void Output2Map(MyMap::ViewOut& out_m) {
+        out_m.clear();
+        std::istringstream iss(output_str);
+        std::string line;
+        while (std::getline(iss, line)) {
+            std::istringstream s(line);
+            std::vector<std::string> lines{ std::istream_iterator<std::string>{s}, std::istream_iterator<std::string>() };
+            out_m.emplace(std::make_pair(lines[0], std::vector<float>{std::stof(lines[1]), std::stof(lines[2]), std::stof(lines[3])}));
+        }
+    };
+};
+
+class IVProductCtrlTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        product_ctrl = std::make_unique<ProductCtrl>(std::move(std::make_unique<StorageText>("../../test/test_data.txt")));
+        product_view = std::make_unique<UnitTestView>();
+        product_ctrl->SetView(product_view.get());
+        data_to_check_true = { "3.4", "3.48", "39.57", "12.7", "4", ".5", "6.",
+        "3,4", "3,48", "39,57", "12,7", "4", ",5" };
+        data_to_check_false = { "absd", "ab,sd", "1..2", "1.,2", ",12,", ",12.", ".12," };
+    };
+    void TearDown() {
+
+    };
+    std::unique_ptr<IVProductCtrl> product_ctrl;
+    std::unique_ptr<IProductView> product_view;
+    std::vector<std::string> data_to_check_true;
+    std::vector<std::string> data_to_check_false;
+};
+
+TEST_F(IVProductCtrlTest, CheckProtein) {
+    for (auto a : data_to_check_true)
+        EXPECT_TRUE(product_ctrl->CheckProtein(a));
+    for (auto a : data_to_check_false)
+        EXPECT_FALSE(product_ctrl->CheckProtein(a));
+}
+
+TEST_F(IVProductCtrlTest, CheckFat) {
+    for (auto a : data_to_check_true)
+        EXPECT_TRUE(product_ctrl->CheckFat(a));
+    for (auto a : data_to_check_false)
+        EXPECT_FALSE(product_ctrl->CheckFat(a));
+}
+
+TEST_F(IVProductCtrlTest, CheckCarbo) {
+    for (auto a : data_to_check_true)
+        EXPECT_TRUE(product_ctrl->CheckCarbo(a));
+    for (auto a : data_to_check_false)
+        EXPECT_FALSE(product_ctrl->CheckCarbo(a));
+}
+
+TEST(IVProductCtrl, AddProduct) {
+    std::unique_ptr<IVProductCtrl> product_ctrl =
+        std::make_unique<ProductCtrl>(std::move(std::make_unique<StorageText>("../../test/test_data.txt")));
+    std::unique_ptr<IProductView> product_view = std::make_unique<UnitTestView>();
+    product_ctrl->SetView(product_view.get());
+
+    EXPECT_TRUE(product_ctrl->AddProduct("Chocolate", "1.", "10,5", "20.0"));
+    EXPECT_FALSE(product_ctrl->AddProduct("Eier", "1.", "10,5", ".0"));
+    EXPECT_FALSE(product_ctrl->AddProduct("mAndel", "1.", "10,5", ".0"));
+    EXPECT_FALSE(product_ctrl->AddProduct("avocado", "1.", "10,5", ".0"));
+    EXPECT_FALSE(product_ctrl->AddProduct("puten", "10..", "10,5", ".0"));
+    EXPECT_FALSE(product_ctrl->AddProduct("ggg", "10..", "10,5", ".0"));
+    product_ctrl->Show();
+    UnitTestView* ut = dynamic_cast<UnitTestView*>(product_view.get());
+
+    MyMap m_out;
+    ut->Output2Map(m_out.data);
+
+    MyMap m_ref{
+        {"avocado", std::vector<float>{ 4.1f, 20.1f, 6.2f }},
+        {"chocolate", std::vector<float>{ 1.0, 10.5, 20.0f }},
+        {"eier", std::vector<float>{12.7f, 11.5f, 0.7f}},
+        {"mandel", std::vector<float>{ 24.0f, 53.0f, 5.7f }}
+    };
+
+    EXPECT_EQ(m_out, m_ref);
+}
+
+TEST(IVProductCtrl, UpdateProduct) {
+    std::unique_ptr<IVProductCtrl> product_ctrl =
+        std::make_unique<ProductCtrl>(std::move(std::make_unique<StorageText>("../../test/test_data.txt")));
+    std::unique_ptr<IProductView> product_view = std::make_unique<UnitTestView>();
+    product_ctrl->SetView(product_view.get());
+
+    EXPECT_TRUE(product_ctrl->UpdateProduct("mandel", IVProductCtrl::Parameter::carbohydrate, "5,9"));
+    EXPECT_FALSE(product_ctrl->UpdateProduct("fff", IVProductCtrl::Parameter::carbohydrate, "20,5"));
+    product_ctrl->Show();
+    MyMap m_out;
+    UnitTestView* ut = dynamic_cast<UnitTestView*>(product_view.get());
+    ut->Output2Map(m_out.data);
+    MyMap m_ref{
+    {"avocado", std::vector<float>{ 4.1f, 20.1f, 6.2f }},
+    {"eier", std::vector<float>{12.7f, 11.5f, 0.7f}},
+    {"mandel", std::vector<float>{ 24.0f, 53.0f, 5.9f }}
+    };
+    EXPECT_EQ(m_out, m_ref);
 }
 
 int main(int argc, char** argv) {
